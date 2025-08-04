2a2736baaf5181572e506554f611dc150faef9d4
Jastxz <javicraft14@gmail.com>
2025-08-04 12:14:51 +0200

    dev: Añadida CORS policy

---
+++ src/main/java/es/jastxz/microPrimeNumbers/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package es.jastxz.microPrimeNumbers.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "https://javig.org/"
+                )
+                .allowedMethods("POST")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
